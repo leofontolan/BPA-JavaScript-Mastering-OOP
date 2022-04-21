604e78598d932439ae3b2eeded402b5e7daf4a38
leofontolan <leofontolan@gmail.com>
2022-04-21 17:10:15 -0300

    Exemplos de Objeto Construtor

---
+++ chap1/3-ObjetosConstrutores.js
@@ -0,0 +1,25 @@
+var johnSmith = {
+  name: "John",
+  surname: "Smith",
+  address: {
+    street: "13  Duncannon Street",
+    city: "London",
+    country: "United Kingdom",
+  },
+  displayFullName: function () {
+    return this.name + " " + this.surname;
+  },
+};
+
+var marioRossi = {
+    name:"Mario",
+    surname:"Rossi",
+    address:{
+        street:"Piazza Colonna 13",
+        city:"Roma",
+        country:"Italy",
+    },
+    displayFullName:function(){
+        return this.name + " " + this.surname;
+    }
+};
