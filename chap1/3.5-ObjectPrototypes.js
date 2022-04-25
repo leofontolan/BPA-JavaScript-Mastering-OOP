bd64c567af51854cb416feca63dcc77171ed5b4b
leofontolan <leofontolan@gmail.com>
2022-04-25 14:34:02 -0300

    Exemplos de Prototipos

---
+++ chap1/3.5-ObjectPrototypes.js
@@ -0,0 +1,6 @@
+var johnSmith = new Person("John", "Smith");
+var marioRossi = new Person("Mario", "Rossi");
+
+// johnSmith.greets = function() {
+//  console.log("Hello " + this.name + " " + this.surname + "!");
+// };
